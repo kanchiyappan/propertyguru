2cc969f24f2e76a1b657dad75fd4052972b0f1b5
kanchiyappan <kanchiyappan.gk@gmail.com>
2013-04-25 14:07:06 +0800

    Update fizzbazztest.php

--- fizzbazztest.php
+++ fizzbazztest.php
@@ -1,52 +1,37 @@
 <?php
-
 require 'FizzBazz.php';
-
 class FizzBazzTest extends PHPUnit_Framework_TestCase {
-    
-    private $_fizzBuzz;
-    
-    public function getFizzBuzz() {
-        
-        if (!$this->_fizzBuzz instanceof FizzBazz) {
-            $this->_fizzBuzz = new FizzBazz();
+    private $checkfizzBuzz;
+    public function getFizzBuzz()
+    {
+        if (!$this->checkfizzBuzz instanceof FizzBazz)
+        {
+            $this->checkfizzBuzz = new FizzBazz();
         }
-        
-        return $this->_fizzBuzz;
+        return $this->checkfizzBuzz;
     }
-
-    public function testrangeFizzBazz() {
-        
+    public function testrangeFizzBazz()
+    {
         $result = $this->getFizzBuzz()->rangeFizzBazz("q","r");        
         $this->assertEquals($this->getFizzBuzz()->nan, $result);
-        
         $result = $this->getFizzBuzz()->rangeFizzBazz(20,10);        
         $this->assertEquals($this->getFizzBuzz()->invalid_range, $result);
-        
         $result = $this->getFizzBuzz()->rangeFizzBazz(12,16);        
         $this->assertEquals('Fizz 13 14 FizzBuzz 16', $result);
-        
         $result = $this->getFizzBuzz()->rangeFizzBazz(1,8);        
         $this->assertEquals('1 2 Fizz 4 Buzz Fizz Bazz 8', $result);
-        
         $result = $this->getFizzBuzz()->rangeFizzBazz(25,32);        
         $this->assertEquals('Buzz 26 Fizz 28 29 FizzBuzz 31 32', $result);
-        
         $result = $this->getFizzBuzz()->rangeFizzBazz(25,25);        
         $this->assertEquals('Buzz', $result);
-        
         $result = $this->getFizzBuzz()->rangeFizzBazz(23,23);        
         $this->assertEquals('23', $result);
-        
         $result = $this->getFizzBuzz()->rangeFizzBazz(30,30);        
         $this->assertEquals('FizzBuzz', $result);
-        
-        $result = $this->getFizzBuzz()->rangeFizzBazz(0,0); //Because 3*0 = 0, and 5*0 = 0       
+        $result = $this->getFizzBuzz()->rangeFizzBazz(0,0);      
         $this->assertEquals('FizzBuzz', $result);
-        
         $result = $this->getFizzBuzz()->rangeFizzBazz(4,11);        
         $this->assertEquals('4 Buzz Fizz Bazz 8 Fizz Buzz Bazz', $result);
-        
         $result = $this->getFizzBuzz()->rangeFizzBazz(4,12);        
         $this->assertEquals('4 Buzz Fizz Bazz 8 Fizz Buzz Bazz Fizz', $result);
     }
