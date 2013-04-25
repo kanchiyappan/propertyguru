9b78a66df4fb1c91f3bc77e1a1482c0717a9dab6
kanchiyappan <kanchiyappan.gk@gmail.com>
2013-04-25 13:53:19 +0800

    Update fizzbuzz.php

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -1,30 +1,26 @@
 <?php
-
 class FizzBuzz {
-    
-    public $invalid_range = "invalid range";
+    public $invalid_range_int = "invalid integer range";
     public $nan = "invalid parameter(s)";
-
     public function rangeFizzBuzz($start, $end) {
-        
         if (!is_int($start) || !is_int($end)) return $this->nan;
-        if ($end < $start) return $this->invalid_range;
-        
+        if ($end < $start) return $this->invalid_range_int;
         $return = "";
-        
-        for ($i = $start; $i <= $end; $i++) {
-            
-            if ($i%3 == 0 && $i%5 == 0) {
+        for ($i = $start; $i <= $end; $i++)
+        {
+            if ($i%3 == 0 && $i%5 == 0)
+            {
                 $return .= " FizzBuzz";
-            } else if ($i%3 == 0) {
+            } else if ($i%3 == 0)
+            {
                 $return .= " Fizz";
-            } else if ($i%5 == 0) {
+            } else if ($i%5 == 0)
+            {
                 $return .= " Buzz";
-            } else /*if ($i%3 != 0 && $i%5 != 0)*/ {
+            } else {
                 $return .= " ".(string)$i;
             }            
         }
-        
         return trim($return);
     }
 }
